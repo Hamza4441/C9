5ddc42640741af73a53542d61b90d3e9228bc830
Hamza4441 <87614278+Hamza4441@users.noreply.github.com>
2021-07-18 17:55:33 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var box1 
+function setup() {
+  createCanvas(400,400);
+  var box = createSprite (250,250,50,50)
+  box1 = createSprite(50,50,50,50)
+}
+
+function draw() 
+{
+  background(30);
+drawSprites ()
+}
+
+
+
+
